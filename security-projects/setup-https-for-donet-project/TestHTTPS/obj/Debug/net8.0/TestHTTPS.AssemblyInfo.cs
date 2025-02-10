a9d59d353ab33da362bc79bd54c5a07bf393fce3
hady <hady@issi.net>
2025-02-10 18:07:32 +0200

    fix

--- security-projects/setup-https-for-donet-project/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
+++ security-projects/setup-https-for-donet-project/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3beffc1891a1d36fdd498413c08e6ec4eab861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a391f900153fd54de720ed42f67afe31db932731")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
