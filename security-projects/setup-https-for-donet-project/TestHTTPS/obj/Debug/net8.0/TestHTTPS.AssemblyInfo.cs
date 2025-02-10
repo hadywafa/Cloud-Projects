a391f900153fd54de720ed42f67afe31db932731
hady <hady@issi.net>
2025-02-10 18:05:58 +0200

    rename

--- security-projects/setup-https-for-donet-project/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
+++ security-projects/setup-https-for-donet-project/TestHTTPS/obj/Debug/net8.0/TestHTTPS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+679324e3c810570d497d88f8af39a644ad601ac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3beffc1891a1d36fdd498413c08e6ec4eab861")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestHTTPS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
